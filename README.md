661937257b1833f0e0fd39ef1ccfa5e8731a340a
熊先生 <z27195322@gmail.com>
2021-04-28 02:16:39 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,71 @@
+[![CircleCI](https://circleci.com/gh/broderickhyman/albiondata-client/tree/master.svg?style=svg)](https://circleci.com/gh/broderickhyman/albiondata-client/tree/master) [![Go Report Card](https://goreportcard.com/badge/github.com/broderickhyman/albiondata-client)](https://goreportcard.com/report/github.com/broderickhyman/albiondata-client)
+
+# Albion Data - Client
+Distributed client for the [Albion Online Data](https://www.albion-online-data.com/)
+project.
+
+A quick note on the legality of this application and if it
+violates the Terms and Conditions for Albion Online. Here is
+the response from SBI when asked if we are allowed to do
+monitor network packets relating to Albion Online:
+> Our position is quite simple. As long as you just look and
+analyze we are ok with it. The moment you modify or manipulate
+something or somehow interfere with our services we will react
+(e.g. perma-ban, take legal action, whatever).
+
+~ MadDave - Technical Lead for Albion Online
+
+Source: https://forum.albiononline.com/index.php/Thread/51604-Is-it-allowed-to-scan-your-internet-trafic-and-pick-up-logs/?postID=512670#post512670
+
+This client monitors local network traffic, identifies UDP packets
+that contain relevant data for Albion Online, and ships the information
+off to a central NATS server that anyone can subscribe to.
+
+[Client download stats](https://www.somsubhra.com/github-release-stats/?username=broderickhyman&repository=albiondata-client)
+
+### Contributing
+This process is run on a [DigitalOcean Droplet](https://www.digitalocean.com) in order to ensure almost perfect uptime and high performance for the users. If you find this project beneficial to you then please consider a donation, thanks!!
+
+[Become a Patron on Patreon!](https://www.patreon.com/bePatron?u=10422119)
+
+[![ko-fi](https://www.ko-fi.com/img/donate_sm.png)](https://ko-fi.com/E1E5K69V)
+
+# Contributions
+Many thanks to the original developers:
+- [Regner](https://github.com/Regner)
+- [pcdummy](https://github.com/pcdummy)
+- [Ultraporing](https://github.com/Ultraporing)
+
+# Downloads
+Downloads can be found here: https://github.com/broderickhyman/albiondata-client/releases
+
+## Running on Mac
+
+1. Download the latest `update-darwin-amd64.gz` file from [the Releases page](https://github.com/broderickhyman/albiondata-client/releases)
+2. Unzip that file from the Finder or with `gunzip update-darwin-amd64.gz`
+3. The unzipped `albiondata-client` file is a Golang binary file. You'll need to make this file executable so it can be run directly. You can do this from your Terminal with: `chmod +x albiondata-client`
+4. Run the client from your Terminal with `./albiondata-client`
+
+# Related Projects
+- [albiondata-deduper-dotNet](https://github.com/BroderickHyman/albiondata-deduper-dotNet)
+- [albiondata-sql-dotNet](https://github.com/BroderickHyman/albiondata-sql-dotNet)
+- [albiondata-api-dotNet](https://github.com/BroderickHyman/albiondata-api-dotNet)
+- [AlbionData.Models](https://github.com/broderickhyman/albiondata-models-dotNet) [![NuGet](https://img.shields.io/nuget/v/AlbionData.Models.svg)](https://www.nuget.org/packages/AlbionData.Models/)
+- [albion-data-website](https://github.com/broderickhyman/albion-data-website)
+
+# Contact Us
+The best way to get in touch with us is on the Albion Online Fansites Discord server in either the #proj-albiondata or the #developers channel. A permanent invite link can be found here: [https://discord.gg/TjWdq24](https://discord.gg/TjWdq24)
+
+# Developer Setup
+### Mac/Linux Setup
+- Install [Dep](https://github.com/golang/dep)
+  - Any OS: `go get -u github.com/golang/dep/cmd/dep`
+  - Mac with Homebrew: `brew install dep`
+- Install dependencies using `dep ensure`
+
+### Windows Setup
+[Windows Setup Guide](https://github.com/broderickhyman/albiondata-client/wiki/Building-in-Windows)
+
+# License
+This project, and all contributed code, are licensed under the MIT
+License. A copy of the MIT License may be found in the repository.
---
+++ albiondata-client.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"flag"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/broderickhyman/albiondata-client/client"
+	"github.com/broderickhyman/albiondata-client/log"
+	"github.com/broderickhyman/albiondata-client/systray"
+	"github.com/broderickhyman/go-githubupdate/updater"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var version string
+
+func init() {
+	// Setup the config file and parse values
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+
+	// if we cannot find the configuration file, set Websockets to false
+	if err != nil {
+		viper.Set("EnableWebsockets", false)
+	}
+
+	client.ConfigGlobal.EnableWebsockets = viper.GetBool("EnableWebsockets")
+	client.ConfigGlobal.AllowedWSHosts = viper.GetStringSlice("AllowedWebsocketHosts")
+
+	flag.BoolVar(
+		&client.ConfigGlobal.Debug,
+		"debug",
+		false,
+		"Enable debug logging.",
+	)
+
+	flag.BoolVar(
+		&client.ConfigGlobal.DisableUpload,
+		"d",
+		false,
+		"If specified no attempts will be made to upload data to remote server.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.ListenDevices,
+		"l",
+		"",
+		"Listen on this comma separated devices instead of all available",
+	)
+
+	flag.BoolVar(
+		&client.ConfigGlobal.LogToFile,
+		"output-file",
+		false,
+		"Enable logging to file.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.OfflinePath,
+		"o",
+		"",
+		"Parses a local file instead of checking albion ports.",
+	)
+
+	flag.BoolVar(
+		&client.ConfigGlobal.Minimize,
+		"minimize",
+		false,
+		"Automatically minimize the window.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.PublicIngestBaseUrls,
+		"i",
+		"nats://public:thenewalbiondata@www.albion-online-data.com:4222",
+		"Base URL to send PUBLIC data to, can be 'nats://', 'http://' or 'noop' and can have multiple uploaders. Comma separated.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.PrivateIngestBaseUrls,
+		"p",
+		"",
+		"Base URL to send PRIVATE data to, can be 'nats://', 'http://' or 'noop' and can have multiple uploaders. Comma separated.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.RecordPath,
+		"record",
+		"",
+		"Enable recording commands to a file for debugging later.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.DebugEventsString,
+		"events",
+		"",
+		"Whitelist of event IDs to output messages when debugging. Comma separated.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.DebugEventsBlacklistString,
+		"events-ignore",
+		"",
+		"Blacklist of event IDs to hide messages when debugging. Comma separated.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.DebugOperationsString,
+		"operations",
+		"",
+		"Whitelist of operation IDs to output messages when debugging. Comma separated.",
+	)
+
+	flag.StringVar(
+		&client.ConfigGlobal.DebugOperationsBlacklistString,
+		"operations-ignore",
+		"",
+		"Blacklist of operation IDs to hide messages when debugging. Comma separated.",
+	)
+
+	flag.BoolVar(
+		&client.ConfigGlobal.DebugIgnoreDecodingErrors,
+		"ignore-decode-errors",
+		false,
+		"Ignore the decoding errors when debugging",
+	)
+}
+
+func main() {
+	flag.Parse()
+
+	if client.ConfigGlobal.Debug {
+		client.ConfigGlobal.LogLevel = "DEBUG"
+	}
+	client.ConfigGlobal.DebugEvents = make(map[int]bool)
+	if client.ConfigGlobal.DebugEventsString != "" {
+		for _, event := range strings.Split(client.ConfigGlobal.DebugEventsString, ",") {
+			number, err := strconv.Atoi(event)
+			if err == nil {
+				client.ConfigGlobal.DebugEvents[number] = true
+			}
+		}
+	}
+	if client.ConfigGlobal.DebugEventsBlacklistString != "" {
+		for _, event := range strings.Split(client.ConfigGlobal.DebugEventsBlacklistString, ",") {
+			number, err := strconv.Atoi(event)
+			if err == nil {
+				client.ConfigGlobal.DebugEvents[number] = false
+			}
+		}
+	}
+
+	client.ConfigGlobal.DebugOperations = make(map[int]bool)
+	if client.ConfigGlobal.DebugOperationsString != "" {
+		for _, operation := range strings.Split(client.ConfigGlobal.DebugOperationsString, ",") {
+			number, err := strconv.Atoi(operation)
+			if err == nil {
+				client.ConfigGlobal.DebugOperations[number] = true
+			}
+		}
+	}
+
+	if client.ConfigGlobal.DebugOperationsBlacklistString != "" {
+		for _, operation := range strings.Split(client.ConfigGlobal.DebugOperationsBlacklistString, ",") {
+			number, err := strconv.Atoi(operation)
+			if err == nil {
+				client.ConfigGlobal.DebugOperations[number] = false
+			}
+		}
+	}
+
+	level, err := logrus.ParseLevel(strings.ToLower(client.ConfigGlobal.LogLevel))
+	if err != nil {
+		log.Errorf("Error getting level: %v", err)
+	}
+
+	log.SetLevel(level)
+
+	if client.ConfigGlobal.OfflinePath != "" {
+		client.ConfigGlobal.Offline = true
+		client.ConfigGlobal.DisableUpload = true
+	}
+
+	if client.ConfigGlobal.DisableUpload {
+		log.Info("Upload is disabled.")
+	}
+
+	startUpdater()
+
+	go systray.Run()
+
+	c := client.NewClient(version)
+	c.Run()
+}
+
+func startUpdater() {
+	if version != "" && !strings.Contains(version, "dev") {
+		u := updater.NewUpdater(
+			version,
+			"broderickhyman",
+			"albiondata-client",
+			"update-",
+		)
+
+		go func() {
+			maxTries := 2
+			for i := 0; i < maxTries; i++ {
+				err := u.BackgroundUpdater()
+				if err != nil {
+					log.Error(err.Error())
+					log.Info("Will try again in 60 seconds. You may need to run the client as Administrator.")
+					// Sleep and hope the network connects
+					time.Sleep(time.Second * 60)
+				} else {
+					break
+				}
+			}
+		}()
+	}
+}
